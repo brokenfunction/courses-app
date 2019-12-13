5a5af4049b5de49736d91ab677428bf5018d80b8
roman1488 <roman.zvyazok@eleks.com>
2019-12-13 16:17:11 +0200

    Initial App Structure

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+    <div className='jumbotron'>
+        <h1>Courses Admin</h1>
+        <p>React/Redux</p>
+        <Link to='about' className='bnt btn-warning btn-lg'>
+            Learn More
+        </Link>
+    </div>
+);
+
+export default HomePage;
